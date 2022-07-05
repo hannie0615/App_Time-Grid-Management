d7f4c58a57898bb351b985b6aae3df372d3e5e73
hannie <shyhannie12@gmail.com>
2022-07-05 21:33:38 +0900

    second commit

--- app/src/main/java/com/example/test01application/DaygridActivity.java
+++ app/src/main/java/com/example/test01application/DaygridActivity.java
@@ -23,9 +23,10 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
-public class DaygridActivity extends AppCompatActivity {
+public class DaygridActivity<cellList> extends AppCompatActivity {
 
     DatabaseReference database = FirebaseDatabase.getInstance().getReference(); // 데베 서버
     DatabaseReference gridnote = database.child("gridnote"); // "gridnote"라는 테이블
@@ -39,6 +40,15 @@ public class DaygridActivity extends AppCompatActivity {
 
     Button btn_apply;
     LinearLayout item_linear;
+    int i=1;
+
+
+    // db 정보를 cellList로 가져올 것!
+
+    List<String> cellcategory = new ArrayList<String>();
+    List<String> celldetail = new ArrayList<String>();
+    List<Integer> celltime = new ArrayList<Integer>();
+    List<Integer> listResId = new ArrayList<Integer>();
 
 
     @Override
@@ -52,6 +62,7 @@ public class DaygridActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         btn_apply = findViewById(R.id.btn_apply);
         item_linear = findViewById(R.id.item_linear);
+
 
         // 뒤로(메인페이지로) 가기
         FloatingActionButton fab_back = (FloatingActionButton) findViewById(R.id.fab_back);
@@ -73,14 +84,7 @@ public class DaygridActivity extends AppCompatActivity {
         layoutManager = new GridLayoutManager(this, 1);
         recyclerView.setLayoutManager(layoutManager);
 
-
-        // db 정보를 cellList로 가져올 것!
         cellList = new ArrayList<>();
-
-        List<String> cellcategory = new ArrayList<String>();
-        List<String> celldetail = new ArrayList<String>();
-        List<Integer> celltime = new ArrayList<Integer>();
-        List<Integer> listResId = new ArrayList<Integer>();
 
         cellcategory.add(INTENT_Category.DO1);
         celldetail.add("이 꽃은 국화");
@@ -88,33 +92,9 @@ public class DaygridActivity extends AppCompatActivity {
         listResId.add(R.drawable.ic_launcher_background);
 
 
-//        gridnote.child("notes").child("1").addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-//                for (DataSnapshot snap : snapshot.getChildren()) {
-//
-//                    GridNote gridNote1 = snap.getValue(GridNote.class);
-//                    String cell_category = gridNote1.getCategory(); // 카테고리
-//                    String cell_detail = gridNote1.getMemo(); // 상세 설명
-//                    Integer cell_time = (int) gridNote1.getTotal_time(); // 총 시간
-//
-//                    cellcategory.add(cell_category);
-//                    celldetail.add(cell_detail);
-//                    celltime.add(cell_time);
-//
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                Toast.makeText(getApplicationContext(), "데이터가 없습니다.", Toast.LENGTH_SHORT).show();
-//            }
-//        });
 
 
-        // 할 일
+        // 할 일 : Firebase 연동하기
         // '연동하기' 버튼 누르면
         // firebase 에서 category, total_time, start_here, start_min 가져와서
         // 해당하는 cellList 번호에 따라
@@ -124,8 +104,10 @@ public class DaygridActivity extends AppCompatActivity {
         btn_apply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                readUser(Integer.toString(i++)); // 여기만  하면 되는데
 
                 Cell cell = new Cell();
+
                 cell.setCell(cellcategory.get(0));
                 cell.setDetail(celldetail.get(0));
                 cell.setTotal_time(celltime.get(0));
@@ -138,27 +120,14 @@ public class DaygridActivity extends AppCompatActivity {
             }
         });
 
-
-        /*
-         * Firebase 연동 부분 추가
-         *
-         * */
-
-
         // Item Click Listener 시작
         adapter.setOnItemClickListener(new CustomCellAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View v, int position) {
                 if (position >= 0) {
-
                     String cell_category = cellList.get(position).getCell();
-                    String cell_detail = cellList.get(position).getDetail();
-                    Integer cell_time = cellList.get(position).getTotal_time();
-                    Integer cell_img = cellList.get(position).getImgId();
-
                     Toast.makeText(DaygridActivity.this, cell_category + "입니다.",
                             Toast.LENGTH_SHORT).show();
-
                     adapter.notifyItemChanged(position);
                 }
             }
@@ -166,43 +135,25 @@ public class DaygridActivity extends AppCompatActivity {
 
     } // onCreate() 끝남
 
+    private void readUser(String userId) {
+        //데이터 읽기
+        database.child("notes").child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                GridNote gridNote1 = snapshot.getValue(GridNote.class);
+                String cell_category = gridNote1.getCategory(); // 카테고리
+                String cell_detail = gridNote1.getMemo(); // 상세 설명
+                Integer cell_time = (int) gridNote1.getTotal_time(); // 총 시간
 
-    private void fillCell() {
-        List<String> cellcategory = Arrays.asList(
-                INTENT_Category.DO1,
-                INTENT_Category.DO2,
-                INTENT_Category.DO3,
-                INTENT_Category.DO4,
-                INTENT_Category.DO5
-        );
-        List<String> celldetail = Arrays.asList(
-                "이 꽃은 국화입니다.",
-                "여기는 사막입니다.",
-                "이 꽃은 수국입니다.",
-                "이 동물은 해파리입니다.",
-                "이 동물은 코알라입니다."
-        );
-        List<Integer> celltime = Arrays.asList(
-                10, 20, 30, 40, 50
-        );
-
-        List<Integer> listResId = Arrays.asList(
-                R.drawable.ic_launcher_background,
-                R.drawable.ic_launcher_foreground,
-                R.drawable.ic_launcher_background,
-                R.drawable.ic_launcher_foreground,
-                R.drawable.ic_launcher_background
-        );
-
-        for (int i = 0; i < 5; i++) {
-            Cell cell = new Cell();
-            cell.setCell(cellcategory.get(i));
-            cell.setDetail(celldetail.get(i));
-            cell.setTotal_time(celltime.get(i));
-            cell.setImgId(listResId.get(i));
-
-            adapter.addItem(cell);
-        }
-        adapter.notifyDataSetChanged();
+                cellcategory.add(cell_category);
+                celldetail.add(cell_detail);
+                celltime.add(cell_time);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { //참조에 액세스 할 수 없을 때 호출
+                Toast.makeText(getApplicationContext(),"데이터를 가져오는데 실패했습니다" , Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
